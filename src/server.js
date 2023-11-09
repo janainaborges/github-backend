ecd9a12ef7845ef883add7088c62f0a1e22fbbb2
Janaina Borges <janainaborgesdr@gmail.com>
2023-11-09 00:50:15 -0300

    adicionado cors

--- src/server.js
+++ src/server.js
@@ -1,15 +1,16 @@
-const express = require('express');
-require('dotenv').config();
-const userRouter = require('./routes/userRoutes');
-const errorHandler = require('./middleware/errorHandler');
+const express = require("express");
+require("dotenv").config();
+const userRouter = require("./routes/userRoutes");
+const errorHandler = require("./middleware/errorHandler");
 
 const app = express();
 const PORT = process.env.PORT || 3000;
-
+const cors = require("cors");
 app.use(express.json());
+app.use(cors());
 
 // Routes
-app.use('/api/users', userRouter);
+app.use("/api/users", userRouter);
 
 // Error handling middleware
 app.use(errorHandler);
@@ -17,3 +18,11 @@ app.use(errorHandler);
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
+const corsOptions = {
+  origin: "http://localhost:3000/",
+  methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+  credentials: true,
+  optionsSuccessStatus: 204,
+};
+
+app.use(cors(corsOptions));
